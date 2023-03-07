469b80733334a495c86decd7a832439b188d0940
simonabrosimov <simonabrosimov@gmail.com>
2023-03-07 17:31:24 +0300

    Task 3.7 Add time counting function

--- index.php
+++ index.php
@@ -25,7 +25,8 @@ $goods = [
         "title" => "2014 Rossignol District Snowboard",
         "category" => $categories["boards"],
         "price" => 10999,
-        "image" => "img/lot-1.jpg"
+        "image" => "img/lot-1.jpg",
+        "expire_date" => '2023-04-01'
 
     ],
 
@@ -33,7 +34,8 @@ $goods = [
         "title" => "DC Ply Mens 2016/2017 Snowboard",
         "category" => $categories["boards"],
         "price" => 159999,
-        "image" => "img/lot-2.jpg"
+        "image" => "img/lot-2.jpg",
+        "expire_date" => '2023-05-13'
 
     ],
 
@@ -41,7 +43,8 @@ $goods = [
         "title" => "Крепления Union Contact Pro 2015 года размер L/XL",
         "category" => $categories["attachment"],
         "price" => 8000,
-        "image" => "img/lot-3.jpg"
+        "image" => "img/lot-3.jpg",
+        "expire_date" => '2023-05-07'
 
     ],
 
@@ -49,7 +52,8 @@ $goods = [
         "title" => "Ботинки для сноуборда DC Mutiny Charocal",
         "category" => $categories["boots"],
         "price" => 10999,
-        "image" => "img/lot-4.jpg"
+        "image" => "img/lot-4.jpg",
+        "expire_date" => '2023-04-01'
 
     ],
 
@@ -57,7 +61,8 @@ $goods = [
         "title" => "Куртка для сноуборда DC Mutiny Charocal",
         "category" => $categories["clothing"],
         "price" => 7500,
-        "image" => "img/lot-5.jpg"
+        "image" => "img/lot-5.jpg",
+        "expire_date" => '2023-04-22'
 
     ],
 
@@ -65,7 +70,8 @@ $goods = [
         "title" => "Маска Oakley Canopy",
         "category" => $categories["other"],
         "price" => 5400,
-        "image" => "img/lot-6.jpg"
+        "image" => "img/lot-6.jpg",
+        "expire_date" => '2023-03-25'
 
     ]
 
@@ -85,6 +91,27 @@ function format_number($initial_number) {
     return $result ." ₽";
 
 };
+
+
+function get_remaining_time($date){
+
+    $end_date = date_create($date);
+    $now_date = date_create();
+    $date_diff = date_diff($end_date, $now_date);
+    $hours_count = date_interval_format($date_diff, "%d-%H-%I");
+    $hours_count_arr = explode("-", $hours_count);
+    
+    $hours = $hours_count_arr[0] * 24 + $hours_count_arr[1];
+    $hours = str_pad($hours, 2, "0", STR_PAD_LEFT);
+    $minutes = intval($hours_count_arr[2]);
+    $minutes = str_pad($minutes, 2, "0", STR_PAD_LEFT);
+
+    $result[] = $hours;
+    $result[] = $minutes;
+
+    return $result;
+};
+
 
 $page_content = include_template('main.php', [
     'categories' => $categories,
--- templates/main.php
+++ templates/main.php
@@ -33,8 +33,10 @@
                             <span class="lot__amount">Стартовая цена</span>
                             <span class="lot__cost"><?=format_number($value["price"]);?></span>
                         </div>
-                        <div class="lot__timer timer">
-                            12:23
+                        <?php $res = get_remaining_time(htmlspecialchars($value["expire_date"]))?>
+                        <div class="lot__timer timer<?php if ($res[0] < 1):?>timer--finishing<?php endif; ?>">
+                            <?= "$res[0]:$res[1]";?>
+                           
                         </div>
                     </div>
                 </div>
